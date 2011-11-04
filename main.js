8dedc81e027b1bd8fa946a098610b71c059d60ce
Luke VanderHart <luke@thinkrelevance.com>
2011-11-04 09:35:22 -0400

    Removed main.js build artifact

--- main.js
+++
@@ -1,6 +0,0 @@
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.string.StringBuffer', 'goog.object', 'goog.array']);
-goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events', 'goog.events.EventTarget', 'goog.events.EventType']);
-goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['cljs.core', 'clojure.browser.event', 'goog.net.XhrIo', 'goog.net.EventType', 'goog.net.xpc.CfgFields', 'goog.net.xpc.CrossPageChannel', 'goog.json']);
-goog.addDependency("../clojure/browser/repl.js", ['clojure.browser.repl'], ['cljs.core', 'clojure.browser.net', 'clojure.browser.event']);
-goog.addDependency("../domina.js", ['domina'], ['cljs.core', 'goog.dom', 'goog.dom.xml', 'clojure.browser.repl']);
-goog.addDependency("../domina/test.js", ['domina.test'], ['cljs.core', 'domina', 'clojure.browser.repl']);

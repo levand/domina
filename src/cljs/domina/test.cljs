1925d0a9150d6e3c6b2c007bbebd2d0a23f6d793
Luke VanderHart <luke@thinkrelevance.com>
2011-11-26 21:13:13 -0500

    Refactored tests to be isolated

--- src/cljs/domina/test.cljs
+++ src/cljs/domina/test.cljs
@@ -27,66 +27,98 @@
          [name (run-test testfn)])
        @tests))
 
-(add-test "reset the page"
-          #(do (destroy (xpath "//body/div"))
-               (append (xpath "//body") "<div class='d1'></div>")
-               (append (xpath "//body/div") "<p class='p1'>P1</p>")
-               (append (xpath "//body/div") "<p class='p2'>P2</p>")
-               (append (xpath "//body/div") "<p id='id1' class='p3'>P3</p>")))
+(defn reset
+  "resets the page"
+  []
+  (destroy (xpath "//body/*")))
+
+(defn standard-fixture
+  "Standard fixture html"
+  []
+  (append (xpath "//body")
+          "<div class='d1'><p class='p1'>P1</p><p class='p2'>P2</p>
+<p id='id1' class='p3'>P3</p>"))
 
 (add-test "basic xpath selection"
-          #(assert (== 3 (count (nodes (xpath "//p"))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 3 (count (nodes (xpath "//p")))))))
 
 (add-test "basic xpath selection (single node)"
-          #(assert (instance? js/Element (single-node (xpath "//p")))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (instance? js/Element (single-node (xpath "//p"))))))
 
 (add-test "xpath selection with class specification"
-          #(assert (== 1 (count (nodes (xpath "//div[@class='d1']"))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 1 (count (nodes (xpath "//div[@class='d1']")))))))
 
 (add-test "a relative xpath expression"
-          #(assert (== 3 (count (nodes (-> (xpath "//body/div[@class='d1']")
-                                           (xpath "p")))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 3 (count (nodes (-> (xpath "//body/div[@class='d1']")
+                                               (xpath "p"))))))))
 (add-test "advanced xpath"
-          #(assert (== 2 (count (nodes (xpath "//p[following-sibling::p[@class='p3']]"))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 2 (count (nodes (xpath "//p[following-sibling::p[@class='p3']]")))))))
 
 (add-test "look up node by id"
-          #(assert (== 1 (count (nodes (id "id1"))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 1 (count (nodes (id "id1")))))))
 
 (add-test "look up nodes by class"
-          #(assert (== 1 (count (nodes (class "p3"))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 1 (count (nodes (class "p3")))))))
 
 (add-test "child selection"
-          #(assert (== 3 (count (children (xpath "//div[@class='d1']"))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 3 (count (children (xpath "//div[@class='d1']")))))))
 
 (add-test "clone a single node"
-          #(assert (== 1 (count (clone (single-node (xpath "//p")))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 1 (count (clone (single-node (xpath "//p"))))))))
 
 (add-test "clone multiple nodes"
-          #(assert (== 3 (count (clone (nodes (xpath "//p")))))))
+          #(do (reset)
+               (standard-fixture)
+               (assert (== 3 (count (clone (nodes (xpath "//p"))))))))
 
 
 (add-test "append a single child to a single parent"
-          #(do (append (xpath "//body") "<p class='appended1'>test</p>")
+          #(do (reset)
+               (append (xpath "//body") "<p class='appended1'>test</p>")
                (assert (== 1 (count (nodes (xpath "//body/p[@class='appended1']")))))))
 
 (add-test "append multiple children to a single parent"
-          #(do (append (xpath "//body")
+          #(do (reset)
+               (append (xpath "//body")
                        "<p class='appended2'>test2-1</p><p class='appended2'>test2-2</p>")
                (assert (== 2 (count (nodes (xpath "//body/p[@class='appended2']")))))))
 
 (add-test "append a single child to multiple parents"
-          #(do (append (xpath "//body/div/p")
+          #(do (reset)
+               (standard-fixture)
+               (append (xpath "//body/div/p")
                        "<span>!!</span>")
                (assert (== 3 (count (nodes (xpath "//div/p/span")))))))
 
 (add-test "append multiple children to multiple parents"
-          #(do (append (xpath "//body/div/p")
+          #(do (reset)
+               (standard-fixture)
+               (append (xpath "//body/div/p")
                        "some <span class='foo'>more</span> text")
                (assert (== 3 (count (nodes (xpath "//div/p/span[@class='foo']")))))
                (assert (== 9 (count (nodes (xpath "//div/p/text()")))))))
 
 (add-test "Insert a single child to a single parent"
-          #(do (append (xpath "//body")
+          #(do (reset)
+               (append (xpath "//body")
                        "<div class='testInserts'></div>")
                (append (xpath "//div[@class='testInserts']")
                        "<p class='i1'></p>")
@@ -99,7 +131,8 @@
                (assert (== 1 (count (nodes (xpath "//p[@class='i2']/following-sibling::*")))))))
 
 (add-test "Insert a single child to multiple parents"
-          #(do (append (xpath "//body")
+          #(do (reset)
+               (append (xpath "//body")
                        "<div class='testInserts' id='testInsert1'></div>")
                (append (xpath "//body")
                        "<div class='testInserts' id='testInsert2'></div>")
@@ -109,39 +142,51 @@
                        "<p class='i3'></p>")
                (insert (xpath "//div[@class='testInserts']")
                        "<p class='i2'></p>" 1)
-               (doseq [children [(xpath "//div[@id='testInsert1']/p")
-                                 (xpath "//div[@id='testInsert2']/p")]]
-                 (assert (== 3 (count (nodes children))))
+               (doseq [children [(xpath "//div[@id='testInsert1']")
+                                 (xpath "//div[@id='testInsert2']")]]
+                 (assert (== 3 (count (nodes (xpath children "p")))))
                  (assert (== 1 (count (nodes (xpath children
                                                     "p[@class='i2']/preceding-sibling::*")))))
                  (assert (== 1 (count (nodes (xpath children
                                                     "p[@class='i2']/following-sibling::*"))))))))
 
 (add-test "destroy a single node"
-          #(do (destroy (xpath "//body/p[@class='appended1']"))
+          #(do (reset)
+               (append (xpath "//body") "<p class='appended1'>app1</p>")
+               (assert (== 1 (count (nodes (xpath "//body/p[@class='appended1']")))))
+               (destroy (xpath "//body/p[@class='appended1']"))
                (assert (== 0 (count (nodes (xpath "//body/p[@class='appended1']")))))))
 
 (add-test "destroy multiple nodes"
-          #(do (destroy (xpath "//body/p[@class='appended2']"))
+          #(do (reset)
+               (append (xpath "//body")
+                       "<p class='appended2'>app1</p><p class='appended2'>app2</p>")
+               (assert (== 2 (count (nodes (xpath "//body/p[@class='appended2']")))))
+               (destroy (xpath "//body/p[@class='appended2']"))
                (assert (== 0 (count (nodes (xpath "//body/p[@class='appended2']")))))))
 
 (add-test "detach and reattach a single node"
-          #(let [n (detach (xpath "//p[@class='p3']"))]
-             (assert (== 0 (count (nodes (xpath "//p[@class='p3']")))))
-             (append (xpath "//div[@class='d1']") n)
-             (assert (== 1 (count (nodes (xpath "//p[@class='p3']")))))))
+          #(do (reset)
+               (standard-fixture)
+               (let [n (detach (xpath "//p[@class='p3']"))]
+                 (assert (== 0 (count (nodes (xpath "//p[@class='p3']")))))
+                 (append (xpath "//div[@class='d1']") n)
+                 (assert (== 1 (count (nodes (xpath "//p[@class='p3']"))))))))
 
 (add-test "detach and reattach multiple nodes"
-          #(let [n (detach (xpath "//div[@class='d1']/p"))]
-             (assert (== 0 (count (nodes (xpath "//div[@class='d1']/p")))))
-             (append (xpath "//div[@class='d1']") n)
-             (assert (== 3 (count (nodes (xpath "//div[@class='d1']/p")))))))
+          #(do (reset)
+               (standard-fixture)
+               (let [n (detach (xpath "//div[@class='d1']/p"))]
+                 (assert (== 0 (count (nodes (xpath "//div[@class='d1']/p")))))
+                 (append (xpath "//div[@class='d1']") n)
+                 (assert (== 3 (count (nodes (xpath "//div[@class='d1']/p"))))))))
 
 (add-test "clear a node's contents"
-          #(do (destroy-children (xpath "//div[@class='d1']"))
+          #(do (reset)
+               (standard-fixture)
+               (destroy-children (xpath "//div[@class='d1']"))
                (assert (== 1 (count (nodes (xpath "//div[@class='d1']")))))
                (assert (== 0 (count (nodes (xpath "//div[@class='d1']/*")))))))
-
 
 (doseq [[name result] (run-tests)]
   (if (not (== result nil))

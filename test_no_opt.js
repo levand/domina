c0b5de3686cc62e1694931e7f730413b935b1a15
Luke VanderHart <luke@thinkrelevance.com>
2011-12-16 15:49:11 -0500

    push build artifacts at Fri Dec 16 20:49:11 UTC 2011

--- test_no_opt.js
+++ test_no_opt.js
@@ -2,5 +2,5 @@ goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.strin
 goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events', 'goog.events.EventTarget', 'goog.events.EventType']);
 goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['cljs.core', 'clojure.browser.event', 'goog.net.XhrIo', 'goog.net.EventType', 'goog.net.xpc.CfgFields', 'goog.net.xpc.CrossPageChannel', 'goog.json']);
 goog.addDependency("../clojure/browser/repl.js", ['clojure.browser.repl'], ['cljs.core', 'clojure.browser.net', 'clojure.browser.event']);
-goog.addDependency("../domina.js", ['domina'], ['goog.dom.classes', 'cljs.core', 'goog.string', 'goog.dom', 'goog.dom.xml', 'clojure.browser.repl', 'goog.style', 'goog.dom.forms']);
+goog.addDependency("../domina.js", ['domina'], ['cljs.core', 'goog.dom', 'goog.dom.xml', 'goog.dom.classes', 'goog.dom.forms', 'goog.style', 'goog.string', 'cljs.core', 'clojure.browser.repl']);
 goog.addDependency("../domina/test.js", ['domina.test'], ['cljs.core', 'domina', 'clojure.browser.repl']);

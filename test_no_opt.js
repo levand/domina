aeaf305ee26e2f6089fecade3a6958a85ee72736
Luke VanderHart <luke@thinkrelevance.com>
2011-12-23 12:58:47 -0500

    push build artifacts at Fri Dec 23 17:58:47 UTC 2011

--- test_no_opt.js
+++ test_no_opt.js
@@ -1,6 +1,6 @@
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events.EventType', 'goog.events.EventTarget', 'goog.events']);
-goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['goog.net.xpc.CrossPageChannel', 'clojure.browser.event', 'goog.net.xpc.CfgFields', 'cljs.core', 'goog.net.EventType', 'goog.json', 'goog.net.XhrIo']);
-goog.addDependency("../clojure/browser/repl.js", ['clojure.browser.repl'], ['clojure.browser.event', 'clojure.browser.net', 'cljs.core']);
-goog.addDependency("../domina.js", ['domina'], ['goog.dom.classes', 'cljs.core', 'goog.string', 'goog.dom', 'goog.dom.xml', 'clojure.browser.repl', 'goog.style', 'goog.dom.forms']);
-goog.addDependency("../domina/test.js", ['domina.test'], ['cljs.core', 'domina', 'clojure.browser.repl']);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.string.StringBuffer', 'goog.object', 'goog.array']);
+goog.addDependency("../clojure/browser/event.js", ['clojure.browser.event'], ['cljs.core', 'goog.events', 'goog.events.EventTarget', 'goog.events.EventType']);
+goog.addDependency("../clojure/browser/net.js", ['clojure.browser.net'], ['cljs.core', 'clojure.browser.event', 'goog.net.XhrIo', 'goog.net.EventType', 'goog.net.xpc.CfgFields', 'goog.net.xpc.CrossPageChannel', 'goog.json']);
+goog.addDependency("../clojure/browser/repl.js", ['clojure.browser.repl'], ['cljs.core', 'clojure.browser.net', 'clojure.browser.event']);
+goog.addDependency("../domina.js", ['domina'], ['cljs.core', 'goog.dom', 'goog.dom.xml', 'goog.dom.classes', 'goog.dom.forms', 'goog.style', 'goog.string', 'cljs.core', 'clojure.browser.repl']);
+goog.addDependency("../domina/test.js", ['domina.test'], ['cljs.core', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'domina', 'clojure.browser.repl']);

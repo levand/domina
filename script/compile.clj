7391ffbe1cdbaa84380083270f2452b35c6ed6b7
Luke VanderHart <luke@thinkrelevance.com>
2011-12-23 12:53:06 -0500

    Fixed compilation bug introduced in a29819

--- script/compile.clj
+++ script/compile.clj
@@ -6,27 +6,27 @@
  (do
    (println "Compiling without any optimiizations...")
    (time
-    (build ["src/cljs" "test/cljs"]
+    (build "test/cljs"
            {:pretty-print *pretty-print*
             :output-dir "public/build_no_opt"
             :output-to "public/test_no_opt.js"}))
    (println "Compiling with whitespace optimizations...")
    (time
-    (build ["src/cljs" "test/cljs"]
+    (build "test/cljs"
            {:optimizations :whitespace
             :pretty-print *pretty-print*
             :output-dir "build/whitespace"
             :output-to "public/test_whitespace.js"}))
    (println "Compiling with simple optimizations...")
    (time
-    (build ["src/cljs" "test/cljs"]
+    (build "test/cljs"
            {:optimizations :simple
             :pretty-print *pretty-print*
             :output-dir "build/simple"
             :output-to "public/test_simple.js"}))
    (println "Compiling with advanced optimizations...")
    (time
-    (build ["src/cljs" "test/cljs"]
+    (build "test/cljs"
            {:optimizations :advanced
             :pretty-print *pretty-print*
             :output-dir "build/advanced"

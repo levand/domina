c4336f600f79622543f45ca63e92ed260f6f8605
Luke VanderHart <luke@thinkrelevance.com>
2012-02-02 20:49:43 -0500

    fix broken build

--- project.clj
+++ project.clj
@@ -4,38 +4,29 @@
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [goog-jar "1.0.0"]]
-  :dev-dependencies [[lein-cljsbuild "0.0.10-SNAPSHOT"]]
+  :dev-dependencies [[lein-cljsbuild "0.0.10"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild [{:source-path "src/cljs"
                :jar true
-               :compiler {:libs ["goog/dom/query.js"]
-                          :pretty-print true
+               :compiler {:pretty-print true
                           :output-dir ".cljsbuild/domina"
                           :output-to "public/domina.js"}}
               {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :pretty-print true
+               :compiler  {:pretty-print true
                            :output-dir "public/build_no_opt"
                            :output-to "public/test_no_opt.js"}}
               {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :optimizations :whitespace
+               :compiler  {:optimizations :whitespace
                            :pretty-print true
                            :output-dir ".cljsbuild/whitespace"
                            :output-to "public/test_whitespace.js"}}
               {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :optimizations :simple
+               :compiler  {:optimizations :simple
                            :pretty-print true
                            :output-dir ".cljsbuild/simple"
                            :output-to "public/test_simple.js"}}
               {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :optimizations :advanced
+               :compiler  {:optimizations :advanced
                            :pretty-print false
                            :output-dir ".cljsbuild/advanced"
                            :output-to "public/test_advanced.js"}}])
-
-
-
-

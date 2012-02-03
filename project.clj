7e48106e5bada96c918bcba4b5c816b79135578e
Bobby Calderwood <pair@thinkrelevance.com>
2012-02-03 15:59:52 -0500

    Fixed project.clj to match upstream.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject domina "0.1.0"
-  :description "A DOM manipulatio library for ClojureScript inspired by JQuery"
+  :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [goog-jar "1.0.0"]]
@@ -7,29 +7,29 @@
   :hooks [leiningen.cljsbuild]
   :cljsbuild [{:source-path "src/cljs"
                :jar true
-               :compiler {#_:libs #_["goog/dom/query.js"]
+               :compiler {:libs ["goog/dom/query.js"]
                           :pretty-print true
                           :output-dir ".cljsbuild/domina"
                           :output-to "public/domina.js"}}
               {:source-path "test/cljs"
-               :compiler  {#_:libs #_["goog/dom/query.js"]
+               :compiler  {:libs ["goog/dom/query.js"]
                            :pretty-print true
                            :output-dir "public/build_no_opt"
                            :output-to "public/test_no_opt.js"}}
               {:source-path "test/cljs"
-               :compiler  {#_:libs #_["goog/dom/query.js"]
+               :compiler  {:libs ["goog/dom/query.js"]
                            :optimizations :whitespace
                            :pretty-print true
                            :output-dir ".cljsbuild/whitespace"
                            :output-to "public/test_whitespace.js"}}
               {:source-path "test/cljs"
-               :compiler  {#_:libs #_["goog/dom/query.js"]
+               :compiler  {:libs ["goog/dom/query.js"]
                            :optimizations :simple
                            :pretty-print true
                            :output-dir ".cljsbuild/simple"
                            :output-to "public/test_simple.js"}}
               {:source-path "test/cljs"
-               :compiler  {#_:libs #_["goog/dom/query.js"]
+               :compiler  {:libs ["goog/dom/query.js"]
                            :optimizations :advanced
                            :pretty-print false
                            :output-dir ".cljsbuild/advanced"

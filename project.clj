1fcbfa2375f360cb1167f9de7d928eb5b8d64514
Luke VanderHart <luke@thinkrelevance.com>
2012-02-26 19:07:22 -0500

    upgrade lein-cljsbuild

--- project.clj
+++ project.clj
@@ -3,38 +3,39 @@
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [goog-jar "1.0.0"]]
-  :dev-dependencies [[lein-cljsbuild "0.0.12"]
+  :plugins [[lein-cljsbuild "0.1.0"]]
+  :dev-dependencies [[lein-cljsbuild "0.0.14"]
                      [lein-clojars "0.6.0"]]
-  ; :hooks [leiningen.cljsbuild]
-  :cljsbuild [{:source-path "src/cljs"
-               :jar true
-               :compiler {:libs ["goog/dom/query.js"]
-                          :pretty-print true
-                          :output-dir ".cljsbuild/domina"
-                          :output-to "public/domina.js"}}
-              {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :pretty-print true
-                           :output-dir "public/build_no_opt"
-                           :output-to "public/test_no_opt.js"}}
-              {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :optimizations :whitespace
-                           :pretty-print true
-                           :output-dir ".cljsbuild/whitespace"
-                           :output-to "public/test_whitespace.js"}}
-              {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :optimizations :simple
-                           :pretty-print true
-                           :output-dir ".cljsbuild/simple"
-                           :output-to "public/test_simple.js"}}
-              {:source-path "test/cljs"
-               :compiler  {:libs ["goog/dom/query.js"]
-                           :optimizations :advanced
-                           :pretty-print true
-                           :output-dir ".cljsbuild/advanced"
-                           :output-to "public/test_advanced.js"}}])
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds [{:source-path "src/cljs"
+                        :jar true
+                        :compiler {:libs ["goog/dom/query.js"]
+                                   :pretty-print true
+                                   :output-dir ".cljsbuild/domina"
+                                   :output-to "public/domina.js"}}
+                       {:source-path "test/cljs"
+                        :compiler  {:libs ["goog/dom/query.js"]
+                                    :pretty-print true
+                                    :output-dir "public/build_no_opt"
+                                    :output-to "public/test_no_opt.js"}}
+                       {:source-path "test/cljs"
+                        :compiler  {:libs ["goog/dom/query.js"]
+                                    :optimizations :whitespace
+                                    :pretty-print true
+                                    :output-dir ".cljsbuild/whitespace"
+                                    :output-to "public/test_whitespace.js"}}
+                       {:source-path "test/cljs"
+                        :compiler  {:libs ["goog/dom/query.js"]
+                                    :optimizations :simple
+                                    :pretty-print true
+                                    :output-dir ".cljsbuild/simple"
+                                    :output-to "public/test_simple.js"}}
+                       {:source-path "test/cljs"
+                        :compiler  {:libs ["goog/dom/query.js"]
+                                    :optimizations :advanced
+                                    :pretty-print true
+                                    :output-dir ".cljsbuild/advanced"
+                                    :output-to "public/test_advanced.js"}}]})
 
 (comment
   (do

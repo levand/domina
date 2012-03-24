ed23837473390be2e176dbaf7a50f3374c9e834d
Luke VanderHart <luke@vanderhart.net>
2012-03-23 21:44:59 -0400

    master always uses SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject domina "1.0.0-beta1"
+(defproject domina "1.0.0-SNAPSHOT"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.3.0"]

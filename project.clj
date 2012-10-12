7a053a7d889ad912ff2b515d258e953c4f7bf4de
Luke VanderHart <luke@vanderhart.net>
2012-10-12 09:52:07 -0400

    update version number for 1.0.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject domina "1.0.2-SNAPSHOT"
+(defproject domina "1.0.1"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.4.0"]]

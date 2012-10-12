ea1aba8cd02fd35b6cd5032210c6e588e526cc6e
Luke VanderHart <luke@vanderhart.net>
2012-10-12 09:51:26 -0400

    bump snapshot number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject domina "1.0.1-SNAPSHOT"
+(defproject domina "1.0.2-SNAPSHOT"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.4.0"]]

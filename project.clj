fa7d6c0e207907fdfc02d6aa30160546420d935f
ckirkendall <ckirkendall@gmail.com>
2013-10-28 19:20:35 -0400

    updated snapshot to 1.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject domina "1.0.2-SNAPSHOT"
+(defproject domina "1.0.3-SNAPSHOT"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-paths ["src/cljs"]
   :dependencies [[org.clojure/clojure "1.4.0"]]

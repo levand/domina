a56361992473a4e05c2388d8f4d36bc693a16651
magomimmo <mimmo.cosenza@gmail.com>
2013-11-03 12:35:08 +0100

    added cljs pathnames to Leiningen test-paths

--- profiles.clj
+++ profiles.clj
@@ -1,37 +1,40 @@
 {:dev {:clean-targets ["out"]
-       :source-paths ["dev-resources/tools/http"]
+       :test-paths ["dev-resources/tools/http" 
+                    "test/cljs" 
+                    "dev-resources/tools/repl"]
+       
        :resources-paths ["dev-resources"]
-
+       
        :dependencies [[com.cemerick/piggieback "0.1.0"]
                       [ring "1.2.0"]
                       [compojure "1.1.5"]]
-
+       
        :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
-
+       
        :cljsbuild
        {:builds {:whitespace
                  {:source-paths ["src/cljs" "test/cljs" "dev-resources/tools/repl"]
                   :compiler  {:optimizations :whitespace
                               :pretty-print true
                               :output-to "dev-resources/public/js/whitespace.js"}}
-
+                 
                  :simple
                  {:source-paths ["src/cljs" "test/cljs"]
                   :compiler  {:optimizations :simple
                               :pretty-print false
                               :output-to "dev-resources/public/js/simple.js"}}
-
+                 
                  :advanced
                  {:source-paths ["src/cljs" "test/cljs"]
                   :compiler  {:optimizations :advanced
                               :pretty-print false
                               :output-to "dev-resources/public/js/advanced.js"}}}
-
+        
         :test-commands {"whitespace"
                         ["phantomjs" :runner "dev-resources/public/js/whitespace.js"]
-
+                        
                         "simple" ["phantomjs" :runner "dev-resources/public/js/simple.js"]
-
+                        
                         "advanced" ["phantomjs" :runner "dev-resources/public/js/advanced.js"]}}
 
        :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}

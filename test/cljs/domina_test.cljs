d96100524c4cad7101061b7caa91c9435c9ced67
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-04 14:44:21 +0100

    removed failing standard cases

--- test/cljs/domina_test.cljs
+++ test/cljs/domina_test.cljs
@@ -5,7 +5,7 @@
             [domina :as dom]))
 
 ;;; nodes
-(deftest nodes-test
+(deftest  nodes-test
   (testing "Unit Testing for (nodes content)\n"
     (testing "Edge Cases\n"
       (testing "(nodes content)"
@@ -18,13 +18,10 @@
              nil (dom/nodes ())
              nil (dom/nodes [])
              nil (dom/nodes {})
-             nil (dom/nodes #{}))))
-    (testing "Standard cases\n"
-      (are [expected actual] (= expected actual)
-           true false))))
+             nil (dom/nodes #{}))))))
 
 ;;; single-node
-(deftest single-node-test
+(deftest  single-node-test
   (testing "Unit Testing for (single-node node)\n"
     (testing "Edge Cases\n"
       (testing "(single-node node)"
@@ -37,13 +34,10 @@
              nil (dom/single-node ())
              nil (dom/single-node [])
              nil (dom/single-node {})
-             nil (dom/single-node #{}))))
-    (testing "Standard cases\n"
-      (are [expected actual] (= expected actual)
-           true false))))
+             nil (dom/single-node #{}))))))
 
 ;;; by-id
-(deftest by-id-test
+(deftest  by-id-test
   (testing "Unit Testing for (by-id id)\n"
     (testing "Edge Cases\n"
       (testing "(by-id s)"
@@ -52,13 +46,10 @@
              nil (dom/by-id "")
              nil (dom/by-id "not-existent-id")
              nil (dom/by-id "not existent id")
-             nil (dom/by-id " "))))
-    (testing "Standard cases\n"
-      (are [expected actual] (= expected actual)
-           true false))))
+             nil (dom/by-id " "))))))
 
 ;;; by-class
-(deftest by-class-test
+(deftest  by-class-test
   (testing "Unit Testing for (by-class class-name)\n"
     (testing "Edge Cases\n"
       (testing "(by-class css-class)"
@@ -67,13 +58,10 @@
              nil (dom/by-class "")
              nil (dom/by-class " ")
              nil (dom/by-class "not-existent-class")
-             nil (dom/by-class "not existent class"))))
-    (testing "Standard cases\n"
-      (are [expected actual] (= expected actual)
-           true false))))
+             nil (dom/by-class "not existent class"))))))
 
 ;;; value
-(deftest value-test
+(deftest  value-test
   (testing "Unit Testing for (value content)\n"
     (testing "Edge Cases\n"
       (testing "(value single-node)"
@@ -82,9 +70,4 @@
              nil (dom/value (dom/by-id ""))
              nil (dom/value (dom/by-id " "))
              nil (dom/value (dom/by-id "not-existent-id"))
-             nil (dom/value (dom/by-id "not existent id")))))
-    (testing "Standard cases\n"
-      (are [expected actual] (= expected actual)
-           true false))))
-
-;;; set-value
+             nil (dom/value (dom/by-id "not existent id")))))))

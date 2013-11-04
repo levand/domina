35b2163e4eeccc69b74b9eb5bd663efcc732c470
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-04 20:25:59 +0100

    added blank? check

--- src/cljs/domina/css.cljs
+++ src/cljs/domina/css.cljs
@@ -11,10 +11,12 @@
 (defn sel
   "Returns content based on a css selector expression. Takes an optional content as a base; if none is given, uses the HTML element as a base."
   ([expr] (sel (root-element) expr))
-  ([base expr] (reify domina/DomContent
-                 (nodes [_] (mapcat #(domina/normalize-seq (dom/query expr %))
-                                    (domina/nodes base)))
-                 (single-node [_] (first (filter
-                                          (complement nil?)
-                                          (mapcat #(domina/normalize-seq (dom/query expr %))
-                                                  (domina/nodes base))))))))
+  ([base expr]
+     (when (not (domina/blank? expr))
+       (reify domina/DomContent
+         (nodes [_] (mapcat #(domina/normalize-seq (dom/query expr %))
+                            (domina/nodes base)))
+         (single-node [_] (first (filter
+                                  (complement nil?)
+                                  (mapcat #(domina/normalize-seq (dom/query expr %))
+                                          (domina/nodes base)))))))))

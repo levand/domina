2116766591f413107bc59194555fb45ff136f300
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-04 13:55:18 +0100

    add edge unit tests for nodes, single-node and value functions

--- test/cljs/domina_test.cljs
+++ test/cljs/domina_test.cljs
@@ -4,26 +4,45 @@
   (:require [cemerick.cljs.test :as t]
             [domina :as dom]))
 
+;;; nodes
 (deftest nodes-test
   (testing "Unit Testing for (nodes content)\n"
     (testing "Edge Cases\n"
-      (testing "(nodes str)"
+      (testing "(nodes content)"
         (are [expected actual] (= expected actual)
-             true false)))
+             nil (dom/nodes nil)
+             nil (dom/nodes "")
+             nil (dom/nodes " ")
+             '("not-a-content") (dom/nodes "not-a-content")
+             '("not a content") (dom/nodes "not a content")
+             nil (dom/nodes ())
+             nil (dom/nodes [])
+             nil (dom/nodes {})
+             nil (dom/nodes #{}))))
     (testing "Standard cases\n"
       (are [expected actual] (= expected actual)
            true false))))
 
+;;; single-node
 (deftest single-node-test
   (testing "Unit Testing for (single-node node)\n"
     (testing "Edge Cases\n"
-      (testing "(single-node str)"
+      (testing "(single-node node)"
         (are [expected actual] (= expected actual)
-             true false)))
+             nil (dom/single-node nil)
+             nil (dom/single-node "")
+             nil (dom/single-node " ")
+             "not-a-node" (dom/single-node "not-a-node")
+             "not a node" (dom/single-node "not a node")
+             nil (dom/single-node ())
+             nil (dom/single-node [])
+             nil (dom/single-node {})
+             nil (dom/single-node #{}))))
     (testing "Standard cases\n"
       (are [expected actual] (= expected actual)
            true false))))
 
+;;; by-id
 (deftest by-id-test
   (testing "Unit Testing for (by-id id)\n"
     (testing "Edge Cases\n"
@@ -38,6 +57,7 @@
       (are [expected actual] (= expected actual)
            true false))))
 
+;;; by-class
 (deftest by-class-test
   (testing "Unit Testing for (by-class class-name)\n"
     (testing "Edge Cases\n"
@@ -51,3 +71,20 @@
     (testing "Standard cases\n"
       (are [expected actual] (= expected actual)
            true false))))
+
+;;; value
+(deftest value-test
+  (testing "Unit Testing for (value content)\n"
+    (testing "Edge Cases\n"
+      (testing "(value single-node)"
+        (are [expected actual] (= expected actual)
+             nil (dom/value (dom/by-id nil))
+             nil (dom/value (dom/by-id ""))
+             nil (dom/value (dom/by-id " "))
+             nil (dom/value (dom/by-id "not-existent-id"))
+             nil (dom/value (dom/by-id "not existent id")))))
+    (testing "Standard cases\n"
+      (are [expected actual] (= expected actual)
+           true false))))
+
+;;; set-value

bc2121216d8c761aa3c2c74374adf22614347ec4
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-05 19:32:24 +0100

    fix clone to handle nil arg

--- src/cljs/domina.cljs
+++ src/cljs/domina.cljs
@@ -144,15 +144,18 @@
 (defn ancestor?
   "Returns true if the first argument is an ancestor of the second argument. Presumes both arguments are single-node contents."
   [ancestor-content descendant-content]
-
-  (when (and ancestor-content descendant-content)
+  (when (and ancestor-content
+             descendant-content
+             (not (empty? ancestor-content))
+             (not (empty? descendant-content)))
     (= (common-ancestor ancestor-content descendant-content)
        (single-node ancestor-content))))
 
 (defn clone
   "Returns a deep clone of content."
   [content]
-  (map #(. % (cloneNode true)) (nodes content)))
+  (when content
+    (map #(. % (cloneNode true)) (nodes content))))
 
 (declare apply-with-cloning)
 

771a14901443bc57f360f2621b49e06c2e3b33bd
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-06 12:24:37 +0100

    name builds

--- profiles.clj
+++ profiles.clj
@@ -1,26 +1,30 @@
 {:dev {:test-paths ["test/cljs"]
        :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
-       :cljsbuild {:builds [{:source-paths ["test/cljs"]
+       :cljsbuild {:builds {:none
+                            {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :pretty-print true
                                          :optimizations :none
                                          :output-dir "public/build_no_opt"
                                          :output-to "public/test_no_opt.js"}}
-                            {:source-paths ["test/cljs"]
+                            :whitespace
+                            {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :optimizations :whitespace
                                          :pretty-print true
                                          :output-dir ".cljsbuild/whitespace"
                                          :output-to "public/test_whitespace.js"}}
-                            {:source-paths ["test/cljs"]
+                            :simple
+                            {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :optimizations :simple
                                          :pretty-print true
                                          :output-dir ".cljsbuild/simple"
                                          :output-to "public/test_simple.js"}}
-                            {:source-paths ["test/cljs"]
+                            :advanced
+                            {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :optimizations :advanced
                                          :pretty-print true
                                          :output-dir ".cljsbuild/advanced"
-                                         :output-to "public/test_advanced.js"}}]}}}
+                                         :output-to "public/test_advanced.js"}}}}}}

811985fbff92d3d5e3ed9f1e69abb26969b3ec5a
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-06 11:14:29 +0100

    upgrade clj to 1.5.1, cljs to 0.0-1847, cljsbuild to 0.3.2, remove unused stuff

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject domina "1.0.3-SNAPSHOT"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
-  :source-paths ["src/cljs"]
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.3.0"]
-            [lein-clojars "0.9.1"]]
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1847"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
   :hooks [leiningen.cljsbuild]
-  :cljsbuild {:builds [{:jar true
-                        :compiler {:libs ["goog/dom/query.js"]
+  :cljsbuild {:builds [{:compiler {:libs ["goog/dom/query.js"]
                                    :pretty-print true
                                    :output-dir ".cljsbuild/domina"
                                    :output-to "public/domina.js"}}
@@ -33,15 +32,4 @@
                                     :optimizations :advanced
                                     :pretty-print true
                                     :output-dir ".cljsbuild/advanced"
-                                    :output-to "public/test_advanced.js"}}]}
-  :repositories {"sonatype-staging"
-                 "https://oss.sonatype.org/content/groups/staging/"})
-
-(comment
-  (do
-    (require '[cljs.repl :as repl])
-    (require '[cljs.repl.browser :as browser])
-    (def env (browser/repl-env))
-    (repl/repl env))
-
-  )
+                                    :output-to "public/test_advanced.js"}}]})

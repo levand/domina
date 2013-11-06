1707a23ac298a4c8a8af3f64d3fb981df82beca3
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-06 20:10:16 +0100

    add test-commands, dev-resources, remove :ouput-dir and modify :output-to

--- profiles.clj
+++ profiles.clj
@@ -1,30 +1,35 @@
 {:dev {:test-paths ["test/cljs"]
+       :dependencies [[hiccups "0.2.0"]]
        :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
        :cljsbuild {:builds {:none
                             {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :pretty-print true
                                          :optimizations :none
-                                         :output-dir "public/build_no_opt"
-                                         :output-to "public/test_no_opt.js"}}
+                                         :output-to "dev-resources/public/js/none.js"}}
                             :whitespace
                             {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :optimizations :whitespace
                                          :pretty-print true
-                                         :output-dir ".cljsbuild/whitespace"
-                                         :output-to "public/test_whitespace.js"}}
+                                         :output-to "dev-resources/public/js/whitespace.js"}}
                             :simple
                             {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :optimizations :simple
                                          :pretty-print true
-                                         :output-dir ".cljsbuild/simple"
-                                         :output-to "public/test_simple.js"}}
+                                         :output-to "dev-resources/public/js/simple.js"}}
                             :advanced
                             {:source-paths ["src/cljs" "test/cljs"]
                              :compiler  {:libs ["goog/dom/query.js"]
                                          :optimizations :advanced
                                          :pretty-print true
-                                         :output-dir ".cljsbuild/advanced"
-                                         :output-to "public/test_advanced.js"}}}}}}
+                                         :output-to "dev-resources/public/js/advanced.js"}}}
+                   :test-commands {"whitespace"
+                                   ["phantomjs" :runner "dev-resources/public/js/whitespace.js"]
+
+                                   "simple"
+                                   ["phantomjs" :runner "dev-resources/public/js/simple.js"]
+
+                                   "advanced"
+                                   ["phantomjs" :runner "dev-resources/public/js/advanced.js"]}}}}

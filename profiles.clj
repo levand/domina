bf2c1fc178029b9ff13a91072beb58ad0a8c4afc
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-06 11:46:48 +0100

    add dev profile

---
+++ profiles.clj
@@ -0,0 +1,26 @@
+{:dev {:test-paths ["test/cljs"]
+       :plugins [[com.cemerick/clojurescript.test "0.1.0"]]
+       :cljsbuild {:builds [{:source-paths ["test/cljs"]
+                             :compiler  {:libs ["goog/dom/query.js"]
+                                         :pretty-print true
+                                         :optimizations :none
+                                         :output-dir "public/build_no_opt"
+                                         :output-to "public/test_no_opt.js"}}
+                            {:source-paths ["test/cljs"]
+                             :compiler  {:libs ["goog/dom/query.js"]
+                                         :optimizations :whitespace
+                                         :pretty-print true
+                                         :output-dir ".cljsbuild/whitespace"
+                                         :output-to "public/test_whitespace.js"}}
+                            {:source-paths ["test/cljs"]
+                             :compiler  {:libs ["goog/dom/query.js"]
+                                         :optimizations :simple
+                                         :pretty-print true
+                                         :output-dir ".cljsbuild/simple"
+                                         :output-to "public/test_simple.js"}}
+                            {:source-paths ["test/cljs"]
+                             :compiler  {:libs ["goog/dom/query.js"]
+                                         :optimizations :advanced
+                                         :pretty-print true
+                                         :output-dir ".cljsbuild/advanced"
+                                         :output-to "public/test_advanced.js"}}]}}}

dc14db9420d1af66b907878513c5fd08e6151836
magomimmo <mimmo.cosenza@gmail.com>
2013-11-06 23:09:45 +0100

    update cljs to r1978 and cljsbuild to 1.0.0-alpha1

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1847"]]
-  :plugins [[lein-cljsbuild "0.3.2"]]
+                 [org.clojure/clojurescript "0.0-1978"]]
+  :plugins [[lein-cljsbuild "1.0.0-alpha1"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds {:deploy
                        {:source-paths ["src/cljs"]

dfe37f713a32cd22ba6e1af6ac1d05762cb5f770
magomimmo <mimmo.cosenza@gmail.com>
2013-11-06 23:15:36 +0100

    update edge unit tests

--- test/cljs/domina/css_edge_test.cljs
+++ test/cljs/domina/css_edge_test.cljs
@@ -2,21 +2,19 @@
   (:require-macros [cemerick.cljs.test :as m :refer (deftest testing are use-fixtures)]
                    [domina.macros :as dm])
   (:require [cemerick.cljs.test :as t]
-            [domina :as dom]
-            [domina.css :as css]
+            [domina :as dom :refer (nodes)]
+            [domina.css :as css :refer (sel)]
             [domina.fixtures :as fix :refer (base-fixture)]))
 
 (use-fixtures :each base-fixture)
 
 ;;; sel
-#_(deftest  sel-test
+(deftest  sel-test
   (testing "Unit Testing for (sel css-expr)\n"
     (testing "Edge Cases\n"
       (are [expected actual] (= expected actual)
-           nil (css/sel nil)
-           nil (css/sel "")
-           nil (css/sel " ")
-           0 (count (dom/nodes (css/sel ".not-existent-css-class")))
-           0 (count (dom/nodes (css/sel "#not-existent-id")))
-           0 (count (dom/nodes (css/sel ".not existent css class")))
-           0 (count (dom/nodes (css/sel "#not existent css id")))))))
+           false (nil? (sel nil))
+           false (nil? (sel ""))
+           false (nil? (sel " "))
+           0 (count (nodes (sel ".not-existent-css-class")))
+           0 (count (nodes (sel "#not-existent-css-id")))))))

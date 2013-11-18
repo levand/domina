901206a662de86983587052c36362eb135662fca
Mimmmo Cosenza <mimmo.cosenza@gmail.com>
2013-11-18 14:07:39 +0100

    add stuff to support austin brepl

---
+++ dev-resources/clj/domina/server.clj
@@ -0,0 +1,23 @@
+(ns domina.server
+  (:require [cemerick.austin.repls :refer (browser-connected-repl-js)]
+            [net.cgrand.enlive-html :as enlive]
+            [compojure.route :refer (resources)]
+            [compojure.core :refer (GET defroutes)]
+            ring.adapter.jetty
+            [clojure.java.io :as io]))
+
+(enlive/deftemplate page
+  (io/resource "public/index.html")
+  []
+  [:body] (enlive/append
+            (enlive/html [:script (browser-connected-repl-js)])))
+
+(defroutes site
+  (resources "/")
+  (GET "/*" req (page)))
+
+(defn run
+  []
+  (defonce server
+    (ring.adapter.jetty/run-jetty #'site {:port 8080 :join? false}))
+  server)

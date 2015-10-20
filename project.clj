571e62a9c7431a4f21ae86b4d3ca635e9d1b9647
Mimmo Cosenza <mimmo.cosenza@gmail.com>
2015-10-20 22:34:52 +0200

    clj=1.7.0, cljs=0.0-3308, cljsbuild=1.1.0 and added clean-targets

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject domina "1.0.3-SNAPSHOT"
   :description "A DOM manipulation library for ClojureScript inspired by JQuery"
   :source-paths ["src/clj" "src/cljs"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2030"]]
-  :plugins [[lein-cljsbuild "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3308"]]
+  :plugins [[lein-cljsbuild "1.1.0"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds {:useless
-                       {:source-paths ["src/cljs"]}}})
+                       {:source-paths ["src/cljs"]}}}
+  :clean-targets ^{:protect false} ["dev-resources/public/js" :target-path])
